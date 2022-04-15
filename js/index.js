1fa259d9e6d727704dbdd68fe8b7775d8e2bddc9
Vinicius <viniciusflexa@gmail.com>
2022-04-15 19:48:07 -0300

    redirect to page :)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 function alertarCodigo(min, max){
+    // cria um codigo aleatorio para o usuario
     var codigo = Math.random() * (max - min) + min
+    // mostra ao usuário seu código
    alert('Seu código é: 2022'+Math.round(codigo)+'! Não esqueça!!!') 
+    // o usuário é mandado de volta a página de login
+   window.location.href = 'index.html'
 }
