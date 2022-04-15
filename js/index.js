a268a2ab29a2949dac0c409c532de1d34c0a681c
Vinicius <viniciusflexa@gmail.com>
2022-04-14 21:07:31 -0300

    login code idea :D

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 function alertarCodigo(min, max){
     var codigo = Math.random() * (max - min) + min
-   alert('Seu código é: '+Math.round(codigo)+'! Não esqueça!!!') 
+   alert('Seu código é: 2022'+Math.round(codigo)+'! Não esqueça!!!') 
 }
